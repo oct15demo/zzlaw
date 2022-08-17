4e543b0c16446ff22b488ec2d393e0430a8e0986
shoop <charles@charless-air-3.home>
2022-08-17 14:58:12 -0400

    formatting output and printing out return values after transformation to python dictionaries

--- legaltexts/aout.py
+++ legaltexts/aout.py
@@ -20,22 +20,15 @@ class XMLValue(Structure):
 
 # Maps of either values or elements.
 # Values are the metadata passed to the function fill_in_data_from_IE_outfile
-# which may be modified, appended to, in the function.
+# which may be modified or appended to in the function.
 # Elements are the attributes of XML citation elements from case10 files
 # The maps are return as lists of key value tupples 
 class valsOrEls(Structure):
     _fields_ = [("valuesMap", POINTER(XMLValue)), ("mapLength", c_int)]
 
-# A list of maps and a year
+# A list of maps and a year, this is the structure embedding the structures above that is returned.
 class valsElsYear(Structure):
     _fields_ = [("valuesMapVector", POINTER(POINTER(valsOrEls))), ("vectorLength", c_int),("year", c_char_p)]
-
-"""py_print_array = lib.print_array
-py_print_array.argtypes = [ctl.ndpointer(np.float64, 
-                                         flags='aligned, c_contiguous'), 
-                           ctypes.c_int]
-A = np.array([1.4,2.6,3.0], dtype=np.float64)"""
-
 
 c_fillindata = "../Debug/zzoflaw"
 makecsv3c = CDLL(c_fillindata)
@@ -60,28 +53,38 @@ t.start("fill_in_c")
 values=fill_in_c(IE_file, values, vallength, txt_file)
 t.end("fill_in_c")
 
-print("\n           Values returned from c code\n\
-____________________________________________________\
-\n    Note: Default value for year is 'False'\n")
-print(f'{"values.year.decode()":1>20}: {values.year.decode()}')
-print(f'{"values.vectorLength":>20}: {values.vectorLength}')
+print("\n    Values returned from c code function fill_in_data_from_IE_outfile, a list of dictionaries and year\n\
+    Note: Default value for year is 'False'\n")
 
-print("\n    Part returned as array of tuple structures:\n")
-#print(f'{values.valuesMapVector[0][0].valuesMap[0].key.decode("utf-8"):>20}')
-#print(f'{values.valuesMapVector[0][0].valuesMap[0].value.decode("utf-8"):>20}')
-#print(f'{values.valuesMapVector[0][0].mapLength:>20}')
+print(f'{"    values.year.decode()":1>20}: {values.year.decode()}')
+print(f'{"    values.vectorLength":>20}: {values.vectorLength}')
+
+print("\n    The key values returned in array of array of tuple structures to mimic unordered_map in C++, dictionary in python:\n")
 
 # Note:The second subscript to access valuesMap is due valuesMapVector holding an array of pointers to arrays of Tupples.
 # See function getValsYear in fillindata.cpp for longer explanation of data structures used.
 
 for i in range(values.vectorLength):
+   print("                  Key Values of Map # " + str(i+1) + "\n") 
    for j in range(values.valuesMapVector[0][0].mapLength):
        print(f'{values.valuesMapVector[i][0].valuesMap[j].key.decode("utf-8"):>20} : {values.valuesMapVector[i][0].valuesMap[j].value.decode("utf-8"):>20}')
 
-valuesDict={}
 
-print("\nTurned into dictionary:\n",valuesDict)
+valsDictionaryList=[]
 
+for i in range(values.vectorLength):
+    valsDictionary={}
+    for j in range(values.valuesMapVector[0][0].mapLength):
+        valsDictionary[values.valuesMapVector[i][0].valuesMap[j].key.decode("utf-8")] = values.valuesMapVector[i][0].valuesMap[j].value.decode("utf-8")
+    valsDictionaryList.append(valsDictionary)
+   
+
+print("\nThe key values after being copied, transformed, and placed into regular python dictionaries.")
+
+for i in range(len(valsDictionaryList)):
+    print("\nDictionary # " + str(i + 1))
+    print(valsDictionaryList[i])
+    
 print("\n")
 t.prRex()
 print(t)
